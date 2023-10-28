f5634ed9a87483f25bfbe886c69881faa6f8577e
Nick <nikolay_makarov_94@mail.ru>
2023-10-29 00:14:56 +0700

    auto with recursion

--- main.cpp
+++ main.cpp
@@ -7,8 +7,16 @@ auto myFunction()
 	return 1;
 }
 
+auto sum(int i) {
+	if (i == 1)
+		return i;           // возвращаемое значение - int
+	else
+		return sum(i - 1) + i;     // теперь можно вызывать рекурсивно
+}
+
 int main()
 {
-	int a = myFunction();
+	int a = sum(5);
+	std::cout << "sum = " << a << std::endl;
 	return 0;
 }
